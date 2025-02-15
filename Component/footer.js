9f3bfea1f98ac04e586098677375194379b03cce
Ravindra Tripathi <140956055+RavindraTripathi@users.noreply.github.com>
2025-02-15 16:52:11 +0530

    Create footer.js

---
+++ Component/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-10 text-center">
+      <p>&copy; 2025 Ravindra Kumar Tripathi. All rights reserved.</p>
+    </footer>
+  );
+}
